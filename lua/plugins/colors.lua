868ad4dc9b7cde2b8b44500169b17cf3cc79b5c2
Ubuntu24 <zc.liu@ed.ac.uk>
2025-10-28 18:28:30 +0000

    Fixed the language servers, added ufo which is for smart (un)folding, fixed the undefined global 'vim' issue.

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,30 +1,40 @@
 local function enable_transparency()
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 end
 
 return {
-    {
-        "folke/tokyonight.nvim",
-        --        config = function()
-        --            vim.cmd.colorscheme "tokyonight"
-        --            enable_transparency()
-        --        end
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-        opts = {
-            theme = 'tokyonight',
-        }
-    },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme "catppuccin"
-        end
-    },
+	{
+		"folke/tokyonight.nvim",
+		--        config = function()
+		--            vim.cmd.colorscheme "tokyonight"
+		--            enable_transparency()
+		--        end
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {
+			theme = "tokyonight",
+		},
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opt = {},
+		config = function()
+			vim.cmd.colorscheme("catppuccin")
+			require("catppuccin").setup({
+				custom_highlights = function()
+					return {
+						-- DiffChange = { fg = "#BD93F9" },
+						-- DiffDelete = { fg = "#FF5555" },
+						netrwMarkFile = { guibg = "#FF5555" },
+					}
+				end,
+			})
+		end,
+	},
 }
